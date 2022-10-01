fb2a41f7abd3cef5d372dc6e621ce4dc312e5aa9
Jose <josealejandrogilmendez08@gmail.com>
2022-09-30 21:44:41 -0400

    create number script
    create function that displays numbers in screen

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('hey');
+const numbers = document.querySelectorAll('.number');
+const screen = document.querySelector('.screen');
+
+numbers.forEach(number => number.addEventListener('click', e=>{
+    if (screen.textContent.length < 15) {
+        screen.textContent += e.target.dataset.key
+    }
+    console.log(e.target.dataset.key);
+}));
+
+const operate = (a, b, operator) =>{
+return (operator == 'sum')? a+b :
+       (operator == 'rest')? a-b :
+       (operator == 'mult')? a*b : a/b
+}
+console.log(operate(2, 2, 'div'));
