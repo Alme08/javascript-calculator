0eace7664d0567782e32469fe856f58c22889705
Jose <josealejandrogilmendez08@gmail.com>
2022-10-02 23:43:48 -0400

    Add keyboard support
    Created function to each button type and add an overflow: hidden to
    calculator screen

--- script.js
+++ script.js
@@ -8,27 +8,37 @@ const calculator = {
     secondNumber: undefined,
     operator: undefined,
     equalPressed: false,
-    dot: false
+    dot: false,
 }
 
-numbers.forEach(number => number.addEventListener('click', e=>{
+const operate = (a, b, operator) =>{
+    if (b === 0 && operator === 'div') return 'XD';
+
+    if(b === undefined) b = a;
+
+    return (operator == 'add')? a+b :
+            (operator == 'rest')? a-b :
+            (operator == 'mult')? a*b : 
+            (operator == 'div')? a/b : screen.textContent;  
+}
+
+const numbersFunction = (number) =>{
     if (screen.textContent.length == 15) return;
     
     if(calculator.equalPressed === true) calculator.firstNumber = 0;
 
     if (calculator.operator === undefined) {
-        screen.textContent = (calculator.firstNumber === 0 && calculator.dot == false)? e.target.dataset.key : screen.textContent + e.target.dataset.key;
+        screen.textContent = (calculator.firstNumber === 0 && calculator.dot == false)? number : screen.textContent + number;
         calculator.firstNumber = parseFloat(screen.textContent);
         calculator.equalPressed = false;
     }else{
-        screen.textContent = (calculator.secondNumber === undefined && calculator.dot == false)? e.target.dataset.key : screen.textContent + e.target.dataset.key;
+        screen.textContent = (calculator.secondNumber === undefined && calculator.dot == false)? number : screen.textContent + number;
         calculator.secondNumber = parseFloat(screen.textContent);
     }
-}));
+}
 
-operators.forEach(operator => operator.addEventListener('click', e=>{
-
-    if (e.target.dataset.operator === 'equal') {
+const operatorFunction = (operator) =>{
+    if (operator === 'equal') {
         let operation = (operate(calculator.firstNumber, calculator.secondNumber, calculator.operator)).toString();
         operation = (operation.length > 15)? operation.slice(0, 15) : operation;
         screen.textContent = operation;
@@ -44,18 +54,18 @@ operators.forEach(operator => operator.addEventListener('click', e=>{
         screen.textContent = operation
         calculator.firstNumber = parseInt(screen.textContent)
         calculator.secondNumber = undefined;
-        calculator.operator = e.target.dataset.operator
+        calculator.operator = operator
         calculator.dot = false;
     }else{
-        calculator.operator = e.target.dataset.operator
+        calculator.operator = operator
         calculator.dot = false;
     }
 
     calculator.equalPressed = false
-}));
+}
 
-methods.forEach(method => method.addEventListener('click', e=>{
-    if(e.target.dataset.method === 'C'){
+const methodsFunction = (method) =>{
+    if(method === 'C'){
 
         calculator.firstNumber = 0;
         screen.textContent = calculator.firstNumber
@@ -64,33 +74,43 @@ methods.forEach(method => method.addEventListener('click', e=>{
         calculator.equalPressed = false;
         calculator.dot = false
 
-    }else if(e.target.dataset.method === 'dot'){
+    }else if(method === 'dot'){
 
         if(screen.textContent.includes('.')) return;
         calculator.dot = true;
         screen.textContent += '.';
         (calculator.operator === undefined)? calculator.firstNumber = parseFloat(screen.textContent) : calculator.secondNumber = parseFloat(screen.textContent);
 
-    }else if(e.target.dataset.method === 'percentage'){
+    }else if(method === 'percentage'){
 
         screen.textContent = parseFloat(screen.textContent/100);
         (calculator.operator === undefined)? calculator.firstNumber = parseFloat(screen.textContent) : calculator.secondNumber = parseFloat(screen.textContent);
 
-    }else if(e.target.dataset.method === 'del'){
+    }else if(method === 'del'){
 
         (screen.textContent.length < 2)? screen.textContent = 0 : screen.textContent = screen.textContent.slice(0,-1);
         (calculator.operator === undefined)? calculator.firstNumber = parseFloat(screen.textContent) : calculator.secondNumber = parseFloat(screen.textContent);
         
     }
-}))
-
-const operate = (a, b, operator) =>{
-    if (b === 0 && operator === 'div') return 'XD';
-
-    if(b === undefined) b = a;
-
-    return (operator == 'add')? a+b :
-            (operator == 'rest')? a-b :
-            (operator == 'mult')? a*b : 
-            (operator == 'div')? a/b : screen.textContent;  
 }
+
+window.addEventListener('keyup', e=>{
+    if(/\d/.test(e.key)){
+        numbersFunction(e.key)
+    }else if(e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/' || e.key === 'Enter'){
+        (e.key === '+')? operatorFunction('add') :
+        (e.key === '-')? operatorFunction('rest') :
+        (e.key === '*')? operatorFunction('mult') :
+        (e.key === '/')? operatorFunction('div') : operatorFunction('equal')
+    }else if(e.key === 'Delete' || e.key === '.' || e.key === '%' || e.key === 'Backspace'){
+        (e.key === 'Delete')? methodsFunction('C') :
+        (e.key === '.')? methodsFunction('dot') :
+        (e.key === '%')? methodsFunction('percentage') : methodsFunction('del')
+    }
+    
+    console.log(e.key);
+})
+
+numbers.forEach(number => number.addEventListener('click', e=> numbersFunction(e.target.dataset.key) ));
+operators.forEach(operator => operator.addEventListener('click', e=> operatorFunction(e.target.dataset.operator)));
+methods.forEach(method => method.addEventListener('click', e=>methodsFunction(e.target.dataset.method)));
